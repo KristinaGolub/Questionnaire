2f649747d5845e200c872ce90d6e3b8c041a7b0b
Голуб Кристина <kristina_golub_2014@mail.ru>
2021-11-15 15:58:56 +0300

    начало формы

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
@@ -23,14 +24,24 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", function (request, response) {
   response.render("home", {
     title: "   ",
-    questions:[
+    questions: [
       {
-        text: 'ААА',
-        type: 1
-      }
-    ]
+        text: "Что есть мы?",
+        type: 1,
+      },
+      {
+        text: "Что есть я?",
+        type: 1,
+      },
+      {
+        text: "Что есть они?",
+        type: 1,
+      },
+    ],
   });
 });
+
+
 
 console.log('http://localhost:3000/')
 app.listen(3000);
